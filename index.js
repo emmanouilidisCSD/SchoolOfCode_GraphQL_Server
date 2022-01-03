dbfe150e1105daf910fa38531da55b1236b83e3e
Manolis Emmanouilidis <emmaemma@csd.auth.gr>
2022-01-03 14:30:20 +0200

    GraphQL-Tools
    Using graphql-tools to create executable schema

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema =  require('./graphql/queries.js');
-const app=express();
-console.log(typeof graphqlHTTP);
-app.use('/graphql',graphqlHTTP({schema: schema.schema, graphiql: true}));
+import  express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { graphql } from 'graphql';
+import schema   from './graphql/queries.mjs';
+import { mergeTypeDefs } from './graphql/typedefs.mjs';
 
-app.listen(4000, () => { 
-    console.log("Running a GraphQL server on http://localhost:4000.");
+const app=express();
+
+
+
+
+console.log(typeof graphqlHTTP);
+const graphQLPath='/graphql';
+const graphQLPort=4000;
+app.use(graphQLPath,graphqlHTTP({schema: schema.schema, graphiql: true}));
+
+app.listen(graphQLPort, () => { 
+    console.log(`Running a GraphQL server on http://localhost:${graphQLPort}${graphQLPath}.`);
 });
